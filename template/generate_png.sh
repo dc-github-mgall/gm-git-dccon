14e7d0827a6d4b2b219ff83ef351ebd896ebb404
Riey <creeper844@gmail.com>
2020-10-10 00:53:20 +0900

    Support customize window-size

--- template/generate_png.sh
+++ template/generate_png.sh
@@ -3,17 +3,21 @@
 SCRIPT_PATH=$(dirname $(realpath $0))
 TEMP_HTML_PATH=$(mktemp --suffix=.html)
 
+if [ -z "$SIZE" ]; then
+    SIZE=100,100
+fi
+
 if [ "$#" -eq 3 ]; then
     sed -e "s/%TEXT1/$1/" -e "s/%TEXT2/$2/" "$SCRIPT_PATH/two-line.html" > "$TEMP_HTML_PATH"
-    chromium --headless --screenshot="$3" --window-size=100,100 "$TEMP_HTML_PATH"
-    rm $TEMP_HTML_PATH
+    chromium --headless --screenshot="$3" --window-size="$SIZE" "$TEMP_HTML_PATH"
+    rm "$TEMP_HTML_PATH"
     exit $?
 fi
 
 if [ "$#" -eq 2 ]; then
     sed -e "s/%TEXT/$1/" "$SCRIPT_PATH/one-line.html" > "$TEMP_HTML_PATH"
-    chromium --headless --screenshot="$2" --window-size=100,100 "$TEMP_HTML_PATH"
-    rm $TEMP_HTML_PATH
+    chromium --headless --screenshot="$2" --window-size="$SIZE" "$TEMP_HTML_PATH"
+    rm "$TEMP_HTML_PATH"
     exit $?
 fi
 
