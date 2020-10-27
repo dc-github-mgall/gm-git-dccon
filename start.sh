5deb7666614c8026ee2b6b3ea9f52c8b5a4d7567
Riey <creeper844@gmail.com>
2020-10-27 13:35:36 +0900

    Add index for image

--- start.sh
+++ start.sh
@@ -5,55 +5,64 @@ mkdir -pv icons
 
 node generate_svg.js
 
-GEN=template/generate_png.sh
+IMG_INDEX=00
 TARGET=output
 
 mkdir -pv $TARGET
 
+function gen() {
+    if [ "$#" -eq 2 ]; then
+        template/generate_png.sh "$1"       "$TARGET/$(printf %02d $IMG_INDEX)-$2.png" &
+    else
+        template/generate_png.sh "$1" "$2"  "$TARGET/$(printf %02d $IMG_INDEX)-$3.png" &
+    fi
+    IMG_INDEX=$(expr $IMG_INDEX + 1)
+}
+
 # Generate list.png
 FONT_SIZE=36px SIZE=70,50 \
-$GEN "Git"                          $TARGET/list.png                    &
+gen "Git"   list
 
-$GEN "git push"                     $TARGET/git_push.png                &
-$GEN "git push -f"                  $TARGET/git_push_f.png              &
+gen "git push"                  git_push
+gen "git push -f"               git_push_f
 
-$GEN "git pull"                     $TARGET/git_pull.png                &
+gen "git pull"                  git_pull
 
-$GEN "git rm -rf"   "."             $TARGET/git_rm_all.png              &
+gen "git rm -rf"   "."          git_rm_all
 
-$GEN "git stash"    "push"          $TARGET/git_stash_push.png          &
-$GEN "git stash"    "pop"           $TARGET/git_stash_pop.png           &
+gen "git stash"    "push"       git_stash_push
+gen "git stash"    "pop"        git_stash_pop
 
-$GEN "git reset"    "--hard"        $TARGET/git_reset_hard.png          &
-$GEN "git reset"    "--mixed"       $TARGET/git_reset_mixed.png         &
-$GEN "git reset"    "--soft"        $TARGET/git_reset_soft.png          &
+gen "git reset"    "--hard"     git_reset_hard
+gen "git reset"    "--mixed"    git_reset_mixed
+gen "git reset"    "--soft"     git_reset_soft
 
-$GEN "git switch"   "master"        $TARGET/git_switch_master.png       &
-$GEN "git switch"   "slave"         $TARGET/git_switch_slave.png        &
-$GEN "git switch"   "프갤"          $TARGET/git_switch_pg.png           &
-$GEN "git switch"   "느그갤"        $TARGET/git_switch_yg.png           &
-$GEN "git switch"   "일베"          $TARGET/git_switch_ib.png           &
-$GEN "git switch"   "루리웹"        $TARGET/git_switch_rw.png           &
+gen "git switch"   "master"     git_switch_master
+gen "git switch"   "slave"      git_switch_slave
+gen "git switch"   "프갤"       git_switch_pg
+gen "git switch"   "느그갤"     git_switch_yg
+gen "git switch"   "일베"       git_switch_ib
+gen "git switch"   "루리웹"     git_switch_rw
 
-$GEN "git tag"      "힙"            $TARGET/git_tag_hipster.png         &
-$GEN "git tag"      "함"            $TARGET/git_tag_functional.png      &
-$GEN "git tag"      "땔"            $TARGET/git_tag_firewood.png        &
-$GEN "git tag"      "찐"            $TARGET/git_tag_wls.png             &
-$GEN "git tag"      "틀"            $TARGET/git_tag_xmf.png             &
-$GEN "git tag"      "씹"            $TARGET/git_tag_tlq.png             &
-$GEN "git tag"      "러"            $TARGET/git_tag_rust.png            &
+gen "git tag"      "힙"         git_tag_hipster
+gen "git tag"      "함"         git_tag_functional
+gen "git tag"      "땔"         git_tag_firewood
+gen "git tag"      "찐"         git_tag_wls
+gen "git tag"      "틀"         git_tag_xmf
+gen "git tag"      "씹"         git_tag_tlq
+gen "git tag"      "러"         git_tag_rust
 
-$GEN "git add"                      $TARGET/git_add.png                 &
-$GEN "git add"      "-A"            $TARGET/git_add_all.png             &
+gen "git add"                   git_add
+gen "git add"      "-A"         git_add_all
 
-$GEN "git commit"                   $TARGET/git_commit.png              &
-$GEN "git clone"                    $TARGET/git_clone.png               &
-$GEN "git merge"                    $TARGET/git_merge.png               &
-$GEN "git blame"                    $TARGET/git_blame.png               &
-$GEN "git fetch"                    $TARGET/git_fetch.png               &
-$GEN "git init"                     $TARGET/git_init.png                &
-$GEN "git diff"                     $TARGET/git_diff.png                &
-$GEN "git log"                      $TARGET/git_log.png                 &
-$GEN "git gc"                       $TARGET/git_gc.png                  &
+gen "git commit"                git_commit
+gen "git clone"                 git_clone
+gen "git merge"                 git_merge
+gen "git blame"                 git_blame
+gen "git fetch"                 git_fetch
+gen "git init"                  git_init
+gen "git diff"                  git_diff
+gen "git log"                   git_log
+gen "git gc"                    git_gc
 
 wait
